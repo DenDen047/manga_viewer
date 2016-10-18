15c1c38e0b4039092679ce8439c5daca03b27e9c
denden047 <naoyashop@yahoo.co.jp>
2016-10-18 12:37:54 +0000

    update create_manga_db.py

--- scripts/create_manga_db.py
+++ scripts/create_manga_db.py
@@ -3,3 +3,16 @@
 import os
 import sys
 import glob
+import MySQLdb
+
+
+root = '../Files'
+
+
+def main():
+    path = os.path.join(root, 'Manga', '*')
+    print(glob.glob(path))
+
+
+if __name__ == "__main__":
+    main()
