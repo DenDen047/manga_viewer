0ed11f4b495fb8cb0e09ba749f4d9560f0a85aae
DenDen047 <sh.mn.nat@gmail.com>
2016-10-19 08:20:46 +0900

    change get_authors() to get_dir_contents() and add db prcessing

--- scripts/create_manga_db.py
+++ scripts/create_manga_db.py
@@ -8,23 +8,29 @@ from init.Init import INIT
 from access_db import AccessDB
 
 
-def get_authors(path):
+def get_dir_contents(path):
     paths = glob.glob(path)
-    authors = []
+    contents = []
     for i in paths:
-        authors.append(i.split('/')[-1])
-    return authors
+        contents.append(i.split('/')[-1])
+    return contents
 
 
 def main():
+    # init
     setPlace = 'setting.json'
     setting = INIT(setPlace)
+    db = AccessDB()
 
     path = os.path.join(setting.pref['root_place'], '*')
-    authors = get_authors(path)
+    authors = get_dir_contents(path)
 
     for i in authors:
         print i
+
+    # end
+    db.commit()
+    db.close()
 
 
 if __name__ == "__main__":
